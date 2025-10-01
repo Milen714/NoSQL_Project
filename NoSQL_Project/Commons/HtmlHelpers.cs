2a8bce9ead63d532b03666bc49652a2fa30ce67c
Milen714 <milensavov1997@gmail.com>
2025-10-02 01:23:39 +0200

    Some Css

---
+++ NoSQL_Project/Commons/HtmlHelpers.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace NoSQL_Project.Commons
+{
+    public static class HtmlHelpers
+    {
+        public static string IsActive(this HtmlHelper html, string controllers = "", string actions = "", string cssClass = "active")
+        {
+            var viewContext = html.ViewContext;
+            var currentController = viewContext.RouteData.Values["controller"] as string;
+            var currentAction = viewContext.RouteData.Values["action"] as string;
+
+            var acceptedControllers = (controllers ?? currentController).Split(',');
+            var acceptedActions = (actions ?? currentAction).Split(',');
+
+            return acceptedControllers.Contains(currentController) && acceptedActions.Contains(currentAction)
+                ? cssClass
+                : string.Empty;
+        }
+    }
+}
